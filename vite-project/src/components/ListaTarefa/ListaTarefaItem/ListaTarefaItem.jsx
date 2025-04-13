f57585ed97974a57e770d47f544e0a7f9897b3b6
Rafael Medeiros <pedro.mape7@gmail.com>
2025-04-13 01:48:16 -0300

    feat: o usuário pode editar o nome de uma tarefa já feita

--- vite-project/src/components/ListaTarefa/ListaTarefaItem/ListaTarefaItem.jsx
+++ vite-project/src/components/ListaTarefa/ListaTarefaItem/ListaTarefaItem.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react";
 import { useAppContext } from "../../../hooks";
 import { Botao, TIPO_BOTAO } from "../../Botao";
 
 import style from "./ListaTarefaItem.module.css";
+import { CampoTexto } from "../../../components";
 
 const ListaTarefaItem = (props) => {
     const { id, nome } = props;
 
-    const { removerTarefa } = useAppContext();
+    const [estaEditando, setEstaEditando] = useState(false);
+
+    const { removerTarefa, editarTarefa } = useAppContext();
 
     return (
         <li className={style.ListaTarefaItem}>
-            { nome }
+            {estaEditando && (
+                <CampoTexto defaultValue={nome} onBlur={() => {setEstaEditando(false);}} autoFocus onChange={(event) => editarTarefa(id, event.currentTarget.value)} />
+            )}
+
+            {!estaEditando && (
+                <span onDoubleClick={() => setEstaEditando(true)}>{nome}</span>
+            )}
+
             <Botao 
             texto='-' 
             tipo={TIPO_BOTAO.SECUNDARIO}
